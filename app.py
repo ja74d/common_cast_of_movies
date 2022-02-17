1fcccec9399ffa52e1667df8e33d8068f84c0622
javad pournosrat <pournosrat.javad@gmail.com>
2022-02-17 12:01:34 +0330

    get full casts

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from bs4 import BeautifulSoup
 source = requests.get('https://www.imdb.com/title/tt0816692/fullcredits/?ref_=tt_cl_sm').text
 soup = BeautifulSoup(source, 'lxml')
 
-for cast in soup.find_all('tr', class_='odd'):
+for cast in soup.find_all('tr', class_='odd') + soup.find_all('tr', class_='even'):
     
     name = cast.find_all('td')[1].text
     print(name)
     
-    role = cast.find('td', class_="character").text
-    print(role)
+    #role = cast.find('td', class_="character").text
+    #print(role)
